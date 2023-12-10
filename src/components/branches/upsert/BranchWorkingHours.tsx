4b31b584a01c2950acfb985a2134d2a4ef3fe270
gabelashvili <gabelashvili1999@gmail.com>
2023-12-10 11:47:51 +0400

    fx

--- src/components/branches/upsert/BranchWorkingHours.tsx
+++ src/components/branches/upsert/BranchWorkingHours.tsx
@@ -38,12 +38,12 @@ const BranchWorkingHours = ({ control, trigger, getValues }: Props) => {
           >
             {[
               'monday' as const,
-              'sunday' as const,
               'tuesday' as const,
               'wednesday' as const,
               'thursday' as const,
               'friday' as const,
               'saturday' as const,
+              'sunday' as const,
             ].map((day) => (
               <TableRow key={day}>
                 <TableCell align="left">
